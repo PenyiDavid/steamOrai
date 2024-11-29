aa2522e407754a3f03fd5919d652d9cdf1eeabe3
Penyaskó Dávid <penyaskodavid@gmail.com>
2024-11-29 09:47:43 +0100

    Initial commit

---
+++ database/migrations/2024_11_29_080716_create_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('game_name');
+            $table->dateTime('release_date');
+            $table->double('rating');
+            $table->tinyInteger('age_restricted');
+            $table->double('price');
+            $table->string('picture_path');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
