ba3b3bf6d06e38efa3f165fdfff25e72d9062a12
Penyaskó Dávid <penyaskodavid@gmail.com>
2024-11-29 10:38:33 +0100

    models

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    //
+    //use HasFactory;
+    protected $fillable = ['game_name', 'release_date', 'rating', 
+        'age_restricted', 'price', 'picture_path', 'description', 'publisher_id'];
+    
+    public function publisher()
+    {
+        return $this->belongsTo(Publisher::class);
+    }
+    
 }
--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Publisher extends Model
 {
-    //
+    protected $fillable = ['publisher_team'];
+    
 }
