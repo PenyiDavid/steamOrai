d6378fb9945385030a09b016b5373248cae70f16
Penyaskó Dávid <penyaskodavid@gmail.com>
2024-12-05 15:43:33 +0100

    update

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 use app\Models\Game;
 use app\Models\Publisher;
 use app\Models\Tag;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Purchase extends Model
+class Purchase extends Pivot
 {
     protected $fillable = ['game_id', 'user_id', 'price'];
 
